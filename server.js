ef619b459da1fbd634d30ab0699936917f238601
KenObul <obulken10@gmail.com>
2025-08-11 13:40:59 +0300

    process.env.port added

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("MongoDB connected successfully");
-    const PORT = 5000;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error("MongoDB Connection Error:", err));
